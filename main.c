fd5a0ddfbbc81dfce1713bd6d6f73765158dd787
HatecrewJP <138243173+HatecrewJP@users.noreply.github.com>
2026-08-16 20:57:28 +0200

    sync

--- main.c
+++ main.c
@@ -41,13 +41,23 @@ typedef struct ipv4_header{
     char Optional[40];
 } ipv4_header;
 
+typedef struct pseudo_tcp_header{
+    unsigned int SrcAddress;
+    unsigned int DestAddress;
+    unsigned char Protocol;
+    unsigned char zero;
+    unsigned short TCPLength;
+    
+} pseudo_tcp_header;
 
 typedef struct tcp_header{
     unsigned short SrcPort;
     unsigned short DestPort;
     unsigned int SequenceNumber;
+    unsigned int AckNumber;
     unsigned char DataOffset : 4;
-    unsigned char Reserved : 4;
+    unsigned char Reserved : 3;
+    unsigned char AccurateECN : 1;
     union {
         unsigned char Flags;
         struct {
@@ -67,6 +77,7 @@ typedef struct tcp_header{
     char Optional[40];
 }tcp_header;
 
+
 static tcp_header CopyTCPHeader(unsigned char *Buffer, int BufferSize){
     tcp_header Result = {};
     Assert(BufferSize >= 20);
@@ -74,16 +85,90 @@ static tcp_header CopyTCPHeader(unsigned char *Buffer, int BufferSize){
     
     Result.SrcPort = (((unsigned short)Buffer[0]) << 8 )| (unsigned short)Buffer[1];
     Result.DestPort = (((unsigned short)Buffer[2]) << 8) | (unsigned short)Buffer[3];
+    Result.SequenceNumber = ((unsigned int) Buffer[4]) << 24 
+        | ((unsigned int)Buffer[5])  << 16
+        | ((unsigned int) Buffer[6]) <<  8 
+        | ((unsigned int)Buffer[7]);
+    Result.AckNumber = ((unsigned int) Buffer[8]) << 24 
+        | ((unsigned int)Buffer[9])  << 16
+        | ((unsigned int) Buffer[10]) <<  8 
+        | ((unsigned int)Buffer[11]);
+    Result.DataOffset = (Buffer[12]>>4) & 0xf;
+    Result.Reserved = Buffer[12] & 0x7;
+    Result.AccurateECN = (Buffer[12]>>3) & 0x1;
+    Result.CWR = Buffer[13] >> 7 & 0x1;
+    Result.ECE = Buffer[13] >> 6 & 0x1;
+    Result.URG = Buffer[13] >> 5 & 0x1;
+    Result.ACK = Buffer[13] >> 4 & 0x1;
+    Result.PSH = Buffer[13] >> 3 & 0x1;
+    Result.RST = Buffer[13] >> 2 & 0x1;
+    Result.SYN = Buffer[13] >> 1 & 0x1;
+    Result.FIN = Buffer[13] >> 0 & 0x1;
+    Result.WindowSize = Buffer[14] | Buffer[15];
+    Result.Checksum = Buffer[16] | Buffer[17];
+    Result.UrgentPointer = Buffer[18] | Buffer [19];
     return Result;
 }
+
+static inline unsigned int ByteSwapU32(unsigned int In){
+    return ((In & 0xff) << 24) | ((In & 0xff00) << 8) | ((In & 0xff0000) >> 8) | ((In & 0xff000000) >>24);
+}
+
+
+static inline unsigned short ByteSwapU16(unsigned short In){
+    return ((In & 0xff) << 8) | ((In & 0xff00) >> 8);
+}
+
+
 
 
 static int VerfiyIPv4Checksum(unsigned char *Buffer, int BufferSize, int Count){
     //Algorithm taken from Wikipedia: https://en.wikipedia.org/wiki/Internet_checksum#Algorithm
     
-    Assert(BufferSize > Count);
+    Assert(BufferSize >= Count);
     int sum = 0;
     unsigned short *Value = (unsigned short*) Buffer;
+    while(Count > 1){
+        sum += *Value++;
+        Count -= 2;
+    }
+    
+    if(Count > 0){
+        sum += (unsigned char)*Buffer;
+    }
+    
+    while(sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+    
+    return (~sum & 0xffff) == 0;
+    
+}
+
+static int VerfiyTCPChecksum(unsigned char *Buffer, 
+                             int BufferSize, 
+                             int Count, 
+                             pseudo_tcp_header PseudoHeaderBE 
+                             //The pseudo Header must be in Network Byte order
+                             )
+{
+    
+    //Algorithm taken from Wikipedia: https://en.wikipedia.org/wiki/Internet_checksum#Algorithm
+    
+    
+    
+    Assert(BufferSize >= Count);
+    int sum = 0;
+    
+    unsigned short *Value = (unsigned short*) &PseudoHeaderBE;
+    int i = sizeof(PseudoHeaderBE);
+    Assert(i % 2 == 0);
+    while(i > 0){
+        sum += *Value++;
+        i-=2;
+    }
+    
+    Value = (unsigned short*) Buffer;
     while(Count > 1){
         sum += *Value++;
         Count -= 2;
@@ -135,6 +220,8 @@ static ipv4_header CopyIPv4Header(unsigned char *Buffer, int BufferSize){
 
 
 int main(){
+    
+    
     WSADATA wsaData;
     int iResult;
     iResult = WSAStartup(MAKEWORD(2,2),&wsaData);
@@ -209,14 +296,27 @@ int main(){
     printf("Source Address: %x\n", Header.SrcAddress);
     printf("Destination Address: %x\n", Header.DestAddress);
     
-    int DataSize = Header.TotalLength - HeaderLengthInBytes;
+    pseudo_tcp_header PseudoHeader = {};
+    PseudoHeader.SrcAddress = ByteSwapU32(Header.SrcAddress);
+    PseudoHeader.DestAddress = ByteSwapU32(Header.DestAddress);
+    PseudoHeader.Protocol = Header.Protocol;
+    PseudoHeader.TCPLength = (Header.TotalLength - HeaderLengthInBytes);
+    
+    
+    int DataSize = PseudoHeader.TCPLength;
     char *Data = malloc(DataSize);
     Assert(Data);
     memcpy(Data,Buffer + HeaderLengthInBytes,DataSize);
     
     
     tcp_header TCPHeader = CopyTCPHeader(Data,DataSize); 
-    
+    int tcp_verfied = VerfiyTCPChecksum(Data, DataSize, DataSize, PseudoHeader);
+    if(tcp_verfied){
+        printf("Src Port: %hu\n", TCPHeader.SrcPort);
+        printf("Dest Port: %hu\n", TCPHeader.DestPort);
+        
+        
+    }
     
     
     
